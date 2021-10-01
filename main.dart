eebda8747f3c5c74d11f4f9c07b4e365a1fe389a
saxenaishii <60481141+saxenaishii@users.noreply.github.com>
2021-10-01 21:49:03 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/bowieapp/generatedblogpagewidget/GeneratedBlogPageWidget.dart';
+import 'package:flutterapp/bowieapp/generatedblogpage2widget/GeneratedBlogPage2Widget.dart';
+import 'package:flutterapp/bowieapp/generatedsocialswidget1/GeneratedSocialsWidget1.dart';
+import 'package:flutterapp/bowieapp/generateddoctor4widget/GeneratedDoctor4Widget.dart';
+import 'package:flutterapp/bowieapp/generateddoctor7widget/GeneratedDoctor7Widget.dart';
+import 'package:flutterapp/bowieapp/generateddoctor9widget/GeneratedDoctor9Widget.dart';
+import 'package:flutterapp/bowieapp/generateddoctor10widget/GeneratedDoctor10Widget.dart';
+import 'package:flutterapp/bowieapp/generateddoctor12widget/GeneratedDoctor12Widget.dart';
+import 'package:flutterapp/bowieapp/generateddoctor6widget/GeneratedDoctor6Widget.dart';
+import 'package:flutterapp/bowieapp/generateddoctor11widget/GeneratedDoctor11Widget.dart';
+import 'package:flutterapp/bowieapp/generateddoctor14widget/GeneratedDoctor14Widget.dart';
+import 'package:flutterapp/bowieapp/generatedbooknowwidget6/GeneratedBOOKNOWWidget6.dart';
+import 'package:flutterapp/bowieapp/generatedandroid1widget/GeneratedAndroid1Widget.dart';
+import 'package:flutterapp/bowieapp/generatedreminder0widget/GeneratedReminder0Widget.dart';
+import 'package:flutterapp/bowieapp/generatedshopwidget/GeneratedShopWidget.dart';
+import 'package:flutterapp/bowieapp/generatedreminderwidget/GeneratedReminderWidget.dart';
+import 'package:flutterapp/bowieapp/generatedreminder2widget/GeneratedReminder2Widget.dart';
+import 'package:flutterapp/bowieapp/generatedloginwidget/GeneratedLoginWidget.dart';
+import 'package:flutterapp/bowieapp/generatedregisterwidget/GeneratedRegisterWidget.dart';
+import 'package:flutterapp/bowieapp/generatedrectangle275widget/GeneratedRectangle275Widget.dart';
+import 'package:flutterapp/bowieapp/generatedcommunitypagewidget/GeneratedCommunityPageWidget.dart';
+import 'package:flutterapp/bowieapp/generatedcommunitypage1widget/GeneratedCommunityPage1Widget.dart';
+import 'package:flutterapp/bowieapp/generatedwelcomewidget/GeneratedWelcomeWidget.dart';
+import 'package:flutterapp/bowieapp/generatedgallerywidget3/GeneratedGalleryWidget3.dart';
+import 'package:flutterapp/bowieapp/generatedandroid2widget/GeneratedAndroid2Widget.dart';
+import 'package:flutterapp/bowieapp/generatedhomepagewidget/GeneratedHomepageWidget.dart';
+import 'package:flutterapp/bowieapp/generateddonationwidget/GeneratedDonationWidget.dart';
+import 'package:flutterapp/bowieapp/generatedgroup1346widget6/GeneratedGroup1346Widget6.dart';
+import 'package:flutterapp/bowieapp/generatedgroup1347widget6/GeneratedGroup1347Widget6.dart';
+
+void main() {
+  runApp(BowieApp());
+}
+
+class BowieApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedBlogPageWidget',
+      routes: {
+        '/GeneratedBlogPageWidget': (context) => GeneratedBlogPageWidget(),
+        '/GeneratedBlogPage2Widget': (context) => GeneratedBlogPage2Widget(),
+        '/GeneratedSocialsWidget1': (context) => GeneratedSocialsWidget1(),
+        '/GeneratedDoctor4Widget': (context) => GeneratedDoctor4Widget(),
+        '/GeneratedDoctor7Widget': (context) => GeneratedDoctor7Widget(),
+        '/GeneratedDoctor9Widget': (context) => GeneratedDoctor9Widget(),
+        '/GeneratedDoctor10Widget': (context) => GeneratedDoctor10Widget(),
+        '/GeneratedDoctor12Widget': (context) => GeneratedDoctor12Widget(),
+        '/GeneratedDoctor6Widget': (context) => GeneratedDoctor6Widget(),
+        '/GeneratedDoctor11Widget': (context) => GeneratedDoctor11Widget(),
+        '/GeneratedDoctor14Widget': (context) => GeneratedDoctor14Widget(),
+        '/GeneratedBOOKNOWWidget6': (context) => GeneratedBOOKNOWWidget6(),
+        '/GeneratedAndroid1Widget': (context) => GeneratedAndroid1Widget(),
+        '/GeneratedReminder0Widget': (context) => GeneratedReminder0Widget(),
+        '/GeneratedShopWidget': (context) => GeneratedShopWidget(),
+        '/GeneratedReminderWidget': (context) => GeneratedReminderWidget(),
+        '/GeneratedReminder2Widget': (context) => GeneratedReminder2Widget(),
+        '/GeneratedLoginWidget': (context) => GeneratedLoginWidget(),
+        '/GeneratedRegisterWidget': (context) => GeneratedRegisterWidget(),
+        '/GeneratedRectangle275Widget': (context) =>
+            GeneratedRectangle275Widget(),
+        '/GeneratedCommunityPageWidget': (context) =>
+            GeneratedCommunityPageWidget(),
+        '/GeneratedCommunityPage1Widget': (context) =>
+            GeneratedCommunityPage1Widget(),
+        '/GeneratedWelcomeWidget': (context) => GeneratedWelcomeWidget(),
+        '/GeneratedGalleryWidget3': (context) => GeneratedGalleryWidget3(),
+        '/GeneratedAndroid2Widget': (context) => GeneratedAndroid2Widget(),
+        '/GeneratedHomepageWidget': (context) => GeneratedHomepageWidget(),
+        '/GeneratedDonationWidget': (context) => GeneratedDonationWidget(),
+        '/GeneratedGroup1346Widget6': (context) => GeneratedGroup1346Widget6(),
+        '/GeneratedGroup1347Widget6': (context) => GeneratedGroup1347Widget6(),
+      },
+    );
+  }
+}
